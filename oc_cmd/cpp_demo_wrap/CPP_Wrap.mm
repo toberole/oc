055d9c538fe0c234f095305d497adbe500520240
zhouwei215664 <zhouwei215664@sogou-inc.com>
2021-06-18 12:20:58 +0800

    note

--- oc_cmd/cpp_demo_wrap/CPP_Wrap.mm
+++ oc_cmd/cpp_demo_wrap/CPP_Wrap.mm
@@ -5,10 +5,13 @@
 
 - (void)test1{
     MoveDemo demo1;
+    printf("address: %p",&demo1);
     MoveDemo demo2(demo1);
+    printf("address: %p",&demo2);
     demo1 = demo2;
-    
+    printf("address: %p",&demo1);
     demo1 = std::move(demo2);
+    printf("address: %p",&demo1);
 }
 
 @end
