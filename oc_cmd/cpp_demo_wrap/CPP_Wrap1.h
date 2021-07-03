4b22267c2679fb5046d6eb1a61739f838e22cc46
zhouwei215664 <zhouwei215664@sogou-inc.com>
2021-07-03 09:40:36 +0800

    note

---
+++ oc_cmd/cpp_demo_wrap/CPP_Wrap1.h
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CPP_Wrap1 : NSObject
+
+-(void)test1;
+
+-(void)test2;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ oc_cmd/cpp_demo_wrap/CPP_Wrap1.mm
@@ -0,0 +1,13 @@
+#import "CPP_Wrap1.h"
+
+@implementation CPP_Wrap1
+
+- (void)test1{
+    
+}
+
+- (void)test2{
+    
+}
+
+@end
